73184d4f520db3bd68b7a283f33fbd4a7b2d9dd2
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-08-12 21:53:31 -0600

    all your base are belong to us

--- users/user-router.js
+++ users/user-router.js
@@ -1,23 +1,35 @@
 const express = require('express');
 
+// once you have refactored this router file to 
+// use your data model code, instead of using
+// knex directly, you can get rid of the following
+// line... it's in the model file, and not
+// needed here.
 const db = require('../data/db-config.js');
+// the knex reference above (through db-config)
+// is replaced by a reference to our data model object:
+// user-model.
+const Users = require('./user-model.js');
 
 const router = express.Router();
 
+// refactored
 router.get('/', async (req, res) => {
   try {
-    const users = await db('users');
+    const users = await Users.find(); 
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: 'Failed to get users' });
   }
 });
 
+// refactored
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const [ user ] = await db('users').where({ id });
+
+    const user = await Users.findById(id);
 
     if (user) {
       res.json(user);
@@ -29,17 +41,31 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// refactored
+router.get('/:id/posts', async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const posts = await Users.findPosts(id);
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({message: 'failed to get posts'});
+  }
+})
+
+// refactored
 router.post('/', async (req, res) => {
   const userData = req.body;
 
   try {
-    const [ id ] = await db('users').insert(userData);
-    res.status(201).json({ created: id });
+    const newUser = await Users.add(userData);
+    res.status(201).json(newUser);
   } catch (err) {
     res.status(500).json({ message: 'Failed to create new user' });
   }
 });
 
+// NOT refactored!!!
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const changes = req.body;
@@ -57,6 +83,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// NOT refactored!!!
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -72,5 +99,7 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: 'Failed to delete user' });
   }
 });
+
+
 
 module.exports = router;
