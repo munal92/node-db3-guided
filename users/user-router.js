5a173a0e0d05339946d5dccf06bc634e60569e24
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-11-18 15:51:52 -0700

    reset master branch

--- users/user-router.js
+++ users/user-router.js
@@ -1,106 +1,80 @@
 const express = require('express');
 
-// once you have refactored this router file to 
-// use your data model code, instead of using
-// knex directly, you can get rid of the following
-// line... it's in the model file, and not
-// needed here.
 const db = require('../data/db-config.js');
-// the knex reference above (through db-config)
-// is replaced by a reference to our data model object:
-// user-model.
-const Users = require('./user-model.js');
 
 const router = express.Router();
 
-// refactored
-router.get('/', async (req, res) => {
-  try {
-    const users = await Users.find(); 
+router.get('/', (req, res) => {
+  db('users')
+  .then(users => {
     res.json(users);
-  } catch (err) {
+  })
+  .catch (err => {
     res.status(500).json({ message: 'Failed to get users' });
-  }
+  });
 });
 
-// refactored
-router.get('/:id', async (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  try {
-
-    const user = await Users.findById(id);
+  db('users').where({ id })
+  .then(users => {
+    const user = users[0];
 
     if (user) {
       res.json(user);
     } else {
       res.status(404).json({ message: 'Could not find user with given id.' })
     }
-  } catch (err) {
+  })
+  .catch(err => {
     res.status(500).json({ message: 'Failed to get user' });
-  }
+  });
 });
 
-// refactored
-router.get('/:id/posts', async (req, res) => {
-  const {id} = req.params;
-
-  try {
-    const posts = await Users.findPosts(id);
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({message: 'failed to get posts'});
-  }
-})
-
-// refactored
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   const userData = req.body;
 
-  try {
-    const newUser = await Users.add(userData);
-    res.status(201).json(newUser);
-  } catch (err) {
+  db('users').insert(userData)
+  .then(ids => {
+    res.status(201).json({ created: ids[0] });
+  })
+  .catch(err => {
     res.status(500).json({ message: 'Failed to create new user' });
-  }
+  });
 });
 
-// NOT refactored!!!
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   const { id } = req.params;
   const changes = req.body;
 
-  try {
-    // update here
-    const user = await Users.update(changes, id);
-
-    // and here
-    if (user) {
-      //and here
-      res.json({ user });
+  db('users').where({ id }).update(changes)
+  .then(count => {
+    if (count) {
+      res.json({ update: count });
     } else {
       res.status(404).json({ message: 'Could not find user with given id' });
     }
-  } catch (err) {
+  })
+  .catch(err => {
     res.status(500).json({ message: 'Failed to update user' });
-  }
+  });
 });
 
-
-// NOT refactored!!!
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
-  try {
-    // update here
-    const count = await Users.remove(id);
-    res.json({message: `${count} records deleted`});
-  } catch (err) {
+  db('users').where({ id }).del()
+  .then(count => {
+    if (count) {
+      res.json({ removed: count });
+    } else {
+      res.status(404).json({ message: 'Could not find user with given id' });
+    }
+  })
+  .catch(err => {
     res.status(500).json({ message: 'Failed to delete user' });
-  }
+  });
 });
-
-
-
 
 module.exports = router;
