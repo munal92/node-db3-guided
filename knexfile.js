d1e07b151a7d2990895a9e2ad033582f8ebce451
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-11-18 23:20:11 -0700

    notes for node-db3-guided

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true, 
+    useNullAsDefault: true,
     connection: {
       filename: './data/blog.db3',
     },
@@ -11,7 +11,14 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
-    // needed when using foreign keys
+    // needed when using foreign keys by default, knex doesn't enforce foreign
+    // key values... that means that by default, if I want to store 9999 in a
+    // foreign key field, but the foreign table doesn't have a record with ID
+    // === 9999, SQLite *will let me*. This can be bad... we want SQLite to
+    // validate that the foreign key we are trying to store actually exists as
+    // the primary key for a record in the foreign table. This bit of code
+    // (stored as the value of the "afterCreate" property) ensures that SQLite
+    // will check and validate for us.
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
